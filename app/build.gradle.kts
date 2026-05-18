12d3cd3dcc15aa7c561ec2d0c1cbf728f1fa6179
Joshua Pogi Bascode <joeycabopomaam@gmail.com>
2026-05-18 14:40:16 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,5 +32,6 @@ android {
 }
 
 dependencies {
-    implementation(project(":core"))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+     implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }

f0b8cf7a2ae09b61813690586d341887a705bcd8
YoureMotherHelped-Love-Mom <joeycabopomaam@gmail.com>
2026-05-20 05:42:05 +0000

    Deploying to repo from @ YoureMotherHelped-Love-Mom/kimcartoon-extension@993ec4c2f8d5fe974469785e14ce2d01aa052bf0 🚀

--- src/main/kotlin/eu/kanade/tachiyomi/extension/all/kimcartoon/CloudflareInterceptor.kt
+++
@@ -1,8 +0,0 @@
-package eu.kanade.tachiyomi.extension.all.kimcartoon
-
-import eu.kanade.tachiyomi.network.interceptor.CloudflareInterceptor
-import okhttp3.OkHttpClient
-
-fun OkHttpClient.Builder.addCloudflareInterceptor(): OkHttpClient.Builder {
-    return addInterceptor(CloudflareInterceptor(debug = true))
-}
